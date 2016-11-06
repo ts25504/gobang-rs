1158eda6497e4dfe4ff68269e3523f08309cb851
ts25504 <ts25504@gmail.com>
2016-11-06 16:25:23 +0800

    add input coordinate.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use std::process::Command;
+use std::io;
+
+fn clear_screen() {
+    let mut child = Command::new("clear").spawn().unwrap();
+    child.wait().unwrap();
+}
 
 struct Board {
     points: Vec<Vec<char>>,
@@ -17,9 +23,7 @@ impl Board {
     }
 
     fn print(&self) {
-        let mut child = Command::new("clear").spawn().unwrap();
-        child.wait().unwrap();
-
+        clear_screen();
         for i in 0..16 {
             for j in 0..16 {
                 print!("{} ", self.points[i][j]);
@@ -37,15 +41,41 @@ impl Board {
     }
 }
 
-struct User {
+struct Game {
     board: Board,
 }
 
-impl User {
-    fn new() -> User {
-        User {
+impl Game {
+    fn new() -> Game {
+        Game {
             board: Board::new(),
         }
+    }
+
+    fn start(&mut self) {
+        clear_screen();
+        println!("Welcome to Gobang game!");
+        loop {
+            let (bx, by) = self.input_coordinate();
+            self.move_black(bx, by);
+
+            let (wx, wy) = self.input_coordinate();
+            self.move_white(wx, wy);
+        }
+    }
+
+    fn input_coordinate(&self) -> (usize, usize) {
+        let mut x = String::new();
+        println!("Move x");
+        io::stdin().read_line(&mut x).unwrap();
+        let ux = x.trim().parse().unwrap();
+
+        let mut y = String::new();
+        println!("Move y");
+        io::stdin().read_line(&mut y).unwrap();
+        let uy = y.trim().parse().unwrap();
+
+        (ux, uy)
     }
 
     fn move_black(&mut self, x: usize, y: usize) {
@@ -60,4 +90,6 @@ impl User {
 }
 
 fn main() {
+    let mut game = Game::new();
+    game.start();
 }
